ed8c28c8ba50727d51bf88eaa69a318724d6fb90
Dmitriy Pismenniy <dima30655@gmail.com>
2020-06-04 00:49:38 +0300

    Updates

---
+++ precache-manifest.128a14c273af88881e679f39a5d1d78e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e521e3d6c4735a5a15a01b889bacd2",
+    "url": "/0206202053/index.html"
+  },
+  {
+    "revision": "efad675ee449ff41ae6f",
+    "url": "/0206202053/static/css/main.7e0cf190.chunk.css"
+  },
+  {
+    "revision": "de84f8c810beffa4364f",
+    "url": "/0206202053/static/js/2.46157661.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/0206202053/static/js/2.46157661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efad675ee449ff41ae6f",
+    "url": "/0206202053/static/js/main.dcd9d7d5.chunk.js"
+  },
+  {
+    "revision": "082efc5946c4b7279ad9",
+    "url": "/0206202053/static/js/runtime-main.eacca6b1.js"
+  },
+  {
+    "revision": "562755dbdf1211c8aaf11817bfa3ce2e",
+    "url": "/0206202053/static/media/delete.562755db.svg"
+  },
+  {
+    "revision": "dbcca1c7fb8dfa0369efed2d0a6e638a",
+    "url": "/0206202053/static/media/photo.dbcca1c7.png"
+  }
+]);
